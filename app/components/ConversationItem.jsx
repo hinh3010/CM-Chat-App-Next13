c57f16fab7fdfa7f54540d56bb44032337be2638
hinhtv <xuanhinh301099@gmail.com>
2023-06-18 23:56:11 +0700

    sidebar is ok

---
+++ app/components/ConversationItem.jsx
@@ -0,0 +1,54 @@
+'use client'
+import Avatar from "./ui/Avatar";
+import InputSearch from "./ui/InputSearch";
+
+const ConversationItem = () => {
+    return (
+        <div
+            className="mt-4 flex items-start justify-between shadow-xl rounded-xl p-6"
+            style={{
+                backgroundColor: '#fcfdff'
+            }}
+        >
+            <div className="flex items-center">
+                <Avatar src={"https://source.unsplash.com/256x256"} />
+                <div
+                    className="flex flex-col ml-4"
+                    style={{
+                        color: '#686768'
+                    }}
+                >
+                    <span
+                        style={{
+                            fontWeight: '800',
+                            fontSize: '1.6rem',
+                            lineHeight: '22px'
+                        }}
+                    >
+                        Adu Adu
+                    </span>
+                    <span
+                        style={{
+                            fontWeight: '600',
+                            fontSize: '1.4rem',
+                            lineHeight: '20px'
+                        }}
+                    >
+                        Adu Adu
+                    </span>
+                </div>
+            </div>
+            <span
+                style={{
+                    fontWeight: '500',
+                    fontSize: '1.2rem',
+                    lineHeight: '16px'
+                }}
+            >
+                9:36
+            </span>
+        </div>
+    );
+}
+
+export default ConversationItem;
--- app/components/Conversations.jsx
+++ app/components/Conversations.jsx
@@ -1,39 +1,35 @@
 'use client'
-import { Switch } from "@headlessui/react";
-import Avatar from "./ui/Avatar";
-import { useState } from "react";
-import Logo from "./ui/Logo";
+import ConversationItem from "./ConversationItem";
 import InputSearch from "./ui/InputSearch";
 
 const Conversations = () => {
-    const [enabled, setEnabled] = useState(true)
     return (
-        <div className="w-full h-full flex flex-col pt-10 pb-20 px-8">
+        <div className="w-full h-full flex flex-col pt-10" style={{ height: '100vh', overflow: 'hidden' }}>
             {/* contact search */}
-            <section className="flex flex-col items-center">
+            <section className="flex flex-col items-center px-8">
                 <InputSearch />
+                <hr style={{ border: '1px solid #333', width: '100%', height: '1px', margin: '10px 0' }} />
             </section >
-            <hr style={{ border: '1px solid #333', width: '100%', height: '1px', margin: '10px 0' }} />
-            <section className="flex flex-col items-center">
-                <section className="flex items-center mt-10">
-                    <Switch
-                        checked={enabled}
-                        onChange={setEnabled}
-                        className={`
-                            ${enabled ? 'bg-blue-600' : 'bg-gray-200'} 
-                            relative inline-flex h-6 w-11 items-center rounded-full
-                        `}
-                    >
-                        <span className="sr-only">Enable notifications</span>
-                        <span
-                            className={`${enabled ? 'translate-x-6' : 'translate-x-1'
-                                } inline-block h-4 w-4 transform rounded-full bg-white transition`}
-                        />
-                    </Switch>
-                </section>
-                <section className="mt-10">
-                    <Avatar src={"https://source.unsplash.com/256x256"} />
-                </section>
+            <section className="flex flex-col px-8"
+                style={{
+                    overflow: 'overlay'
+                }}
+            >
+
+                <ConversationItem />
+                <ConversationItem />
+                <ConversationItem />
+                <ConversationItem />
+                <ConversationItem />
+                <ConversationItem />
+                <ConversationItem />
+                <ConversationItem />
+                <ConversationItem />
+                <ConversationItem />
+                <ConversationItem />
+                <ConversationItem />
+                <ConversationItem />
+                <ConversationItem />
             </section>
         </div >
     );
